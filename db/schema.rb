adbc14e37c03345ecb22f64779aa3806320d55a0
uk1006git <u_kgame.daba@icloud.com>
2023-11-29 12:16:04 +0900

    チャットルーム名の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_26_193246) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_022607) do
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -35,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_26_193246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
